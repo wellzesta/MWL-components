e027a4b2aebddc9248bb6a269d8bc245eef33f5b
John Robinson <john.robinson@wellzesta.com>
2016-01-28 09:15:51 -0500

    unit tests are passing

--- app/register/route.js
+++ app/register/route.js
@@ -7,8 +7,8 @@ export default Ember.Route.extend({
 				name: this.controller.get('name')
 			});
 			user.save().then(() => {
-        this.controller.set('message','A new user with the name "' 
-          + this.controller.get('name') + '" was added!');
+        this.controller.set('message','A new user with the name "' + this.controller.get('name') /
+        	+ '" was added!');          
         this.controller.set('name',null);
 // Go ahead and go to https://mwl-components.firebaseio.com/ to verify that the user has been added
 			}, function() {
